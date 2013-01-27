54743ae5a2db5ec1b3ce91dea8c5f61e6d6a8c6d
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-01-27 14:42:12 +0400

    Init

---
+++ AliensOldEnemies/AliensOldEnemies/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace AliensOldEnemies
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
